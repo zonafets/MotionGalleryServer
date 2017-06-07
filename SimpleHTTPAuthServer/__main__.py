80c49b62f2de5dad97773f9c48f25daab8870c75
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-06-06 22:21:23 -0400

    rename arguement to https and fix SSL_CMD

--- SimpleHTTPAuthServer/__main__.py
+++ SimpleHTTPAuthServer/__main__.py
@@ -14,7 +14,8 @@ from . import __prog__
 
 CERT_FILE = os.path.expanduser("~/.ssh/cert.pem")
 KEY_FILE = os.path.expanduser("~/.ssh/key.pem")
-SSL_CMD = "openssl req -newkey rsa:2048 -new -nodes -keyout {0} -out {1}".format(KEY_FILE, CERT_FILE)
+SSL_CMD = "openssl req -newkey rsa:2048 -new -nodes -x509 "\
+            "-days 3650 -keyout {0} -out {1}".format(KEY_FILE, CERT_FILE)
 
 class SimpleHTTPAuthHandler(SimpleHTTPRequestHandler):
     ''' Main class to present webpages and authentication. '''
@@ -47,11 +48,11 @@ class SimpleHTTPAuthHandler(SimpleHTTPRequestHandler):
             self.wfile.write(self.headers.getheader('Authorization'))
             self.wfile.write('not authenticated')
 
-def serve_https(https_port=80, cert=True, start_dir=None, handler_class=SimpleHTTPAuthHandler):
+def serve_https(https_port=80, https=True, start_dir=None, handler_class=SimpleHTTPAuthHandler):
     ''' setting up server '''
     httpd = SocketServer.TCPServer(("", https_port), handler_class)
 
-    if cert:
+    if https:
         httpd.socket = ssl.wrap_socket(httpd.socket, keyfile=KEY_FILE,
                                        certfile=CERT_FILE, server_side=True)
 
@@ -69,11 +70,12 @@ def main():
     parser.add_argument('port', type=int, help='port number')
     parser.add_argument('key', help='username:password')
     parser.add_argument('--dir', required=False, help='directory')
-    parser.add_argument('--cert', help='Use the cert', action='store_true', default=False)
+    parser.add_argument('--https', help='Use https', action='store_true', default=False)
     args = parser.parse_args()
 
-    if args.cert:
+    if args.https:
         if not (os.path.exists(CERT_FILE) and os.path.exists(KEY_FILE)):
+            print >>sys.stderr
             print >>sys.stderr, "Missing {} or {}".format(CERT_FILE, KEY_FILE)
             print >>sys.stderr, "Run `{}`".format(SSL_CMD)
             print >>sys.stderr
@@ -81,7 +83,7 @@ def main():
 
     SimpleHTTPAuthHandler.KEY = base64.b64encode(args.key)
 
-    serve_https(int(args.port), cert=args.cert,
+    serve_https(int(args.port), https=args.https,
                 start_dir=args.dir, handler_class=SimpleHTTPAuthHandler)
 
 
